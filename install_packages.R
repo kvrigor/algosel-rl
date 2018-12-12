ef671a4603154248cb45f3ede675fd64cbfdb251
kvrigor <kvrigor@users.noreply.github.com>
2018-12-12 14:10:01 +0800

    Added script that can automatically install all the required packages

---
+++ install_packages.R
@@ -0,0 +1,22 @@
+# Core machine learning packages 
+install.packages("aslib")
+install.packages("llama")
+install.packages("randomForest")
+install.packages("tensorflow")
+install.packages("parallelMap")
+
+# Packages for tidying data, plotting, and reporting
+install.packages("tidyr")
+install.packages("dplyr")
+install.packages("purrr")
+install.packages("magrittr")
+install.packages("scales")
+install.packages("ggplot2")
+install.packages("ggthemes")
+install.packages("rmarkdown")
+
+
+
+
+
+
